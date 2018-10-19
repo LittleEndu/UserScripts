2ed5534579c519b868b2accde5b7eca4ad900216
Endrik <enduslittlecomputer@gmail.com>
2018-10-19 21:55:52 +0300

    I think nobody even used this besides me...

--- LHReader.user.js
+++ LHReader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autoreader for Lhtranslation
-// @version      2.1
-// @description  Inserts a button which takes you to reader automatically.
+// @version      3.0
+// @description  Plugin broken...
 // @author       LittleEndu
 // @include      http://lhtranslation.net/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
@@ -11,34 +11,7 @@
 $(function() {
     // Insert our buttons into each article
     $('#content').find('article').each(function (index){
-        var url = $(this).find('.readMore').find('a').attr('href'); // url for AJAX
-        var $button = $('<div class=readMore><a href="javascript:void(0)" style="width: 155px;">Find online reader</a></div>');
-        $button.click(function(){
-            $button.find('a').text('Searching');
-            // Find the reader
-            $.ajax({
-                url: url,
-                success: function(data){
-                    var reader = null;
-                    $(data).find('.commentlist').find('a').each(function(){
-                    if (this.href.match(/https?:\/\/read\.lhtranslation\.com\/.*/))
-                        reader = this.href;
-                        return false;
-                    });
-                    if (reader){
-                        $button.find('a').attr('href', reader);
-                        $button.find('a').text('Read online NOW');
-                    }else{
-                        $button.find('a').text('No reader :\'(');
-                    }
-                },
-                error: function(xhr){
-                    $button.find('a').text(xhr.status + ' Error');
-                }
-            });
-            // Make button only clickable once
-            $button.off('click');
-        });
+        var $button = $('<div class=readMore><a href="javascript:void(0)" style="width: 155px;">New disqus broke plugin :(</a></div>');
         $(this).append($button);
     });
     // Apply our css
